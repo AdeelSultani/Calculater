8f17421290723e3fe99c4e98c1a074944bfa01ce
Win 10 <adeelsultani68@gmail.com>
2025-07-22 11:52:17 -0700

    Second commit

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -14,6 +14,7 @@ class _splashState extends State<Splashscreen> {
 
   @override
   void initState() {
+    ///timer function called
     Timer(Duration(seconds: 2), () {
       Navigator.pushReplacement(context,
           MaterialPageRoute(builder: (context) => Caluculaterscreen()));
